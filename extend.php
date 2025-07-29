c9ad7b4ca3e927ee6515749b77284ed2255676e1
zDaleZ <77269678+zDaleZ@users.noreply.github.com>
2025-07-29 13:34:15 +0800

    Fix & add
    Fix CVE.
    Add stickers.

--- extend.php
+++ extend.php
@@ -5,7 +5,7 @@ use s9e\TextFormatter\Configurator;
 return (new Extend\Formatter)
         ->configure(function (Configurator $config){
             $config->BBCodes->addCustom(
-                '[tieba={TEXT1}]',
-                '<img class="tieba_stickers" src="/assets/extensions/wvbdev-tieba-stickers/{TEXT1}.png" width="30px"/>',
+                '[tieba={SIMPLETEXT1}]',
+                '<img class="tieba_stickers" src="/assets/extensions/wvbdev-tieba-stickers/{SIMPLETEXT1}.png" width="30px"/>',
             );
         });
